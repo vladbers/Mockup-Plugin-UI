4094b2049d0e55ef9f718028a8673cee4bee9323
VladBers <stasgalanin@yandex.ru>
2021-11-22 18:36:55 +0300

    fix errors

--- src/UI/components/banner.jsx
+++ src/UI/components/banner.jsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
 import Alert from '@mui/material/Alert';
-import Collapse from '@mui/material/Collapse';
 import {makeStyles} from "@mui/styles";
 import {Typography} from "@mui/material";
 import {styled} from "@mui/material/styles";
